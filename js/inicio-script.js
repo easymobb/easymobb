7667ec8c322de73b27ab2b4c9a3ce0f91232b445
ThiagoMullerR <thiago.m.rodrigues@outlook.com>
2020-08-06 18:42:32 -0300

    identação

--- js/inicio-script.js
+++ js/inicio-script.js
@@ -2,10 +2,10 @@
 var h2P = document.querySelector('#be8f8601-90cc-47ca-b66a-be1c3fd338d1');
 var botaoP = document.querySelector('#df087bc2-af6a-4bcc-956b-224420f79a61');
 
-  h2P.setAttribute('data-aos','fade-down');
-  h2P.setAttribute('data-aos-delay','150');
-  h2P.setAttribute('data-aos-duration','1500');
+h2P.setAttribute('data-aos','fade-down');
+h2P.setAttribute('data-aos-delay','150');
+h2P.setAttribute('data-aos-duration','1500');
 
-  botaoP.setAttribute('data-aos','fade-down');
-  h2P.setAttribute('data-aos-delay','250');
-  botaoP.setAttribute('data-aos-duration','1000');
+botaoP.setAttribute('data-aos','fade-down');
+h2P.setAttribute('data-aos-delay','250');
+botaoP.setAttribute('data-aos-duration','1000');
