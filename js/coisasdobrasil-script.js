e23753ea47f7a050e70d6049535789272b9257f4
ThiagoMullerR <thiago.m.rodrigues@outlook.com>
2020-08-10 16:37:07 -0300

    js

--- js/coisasdobrasil-script.js
+++ js/coisasdobrasil-script.js
@@ -1,8 +1,16 @@
 // Animação AOS
 var agaveTexto = document.querySelector("[id='e78a7e30-d48f-4d81-84c0-715a2173b311']");
 var baobaTexto = document.querySelector("[id='07783d72-416a-4974-8335-e3e6aa8979aa']");
+var baobaTexto = document.querySelector("[id='07783d72-416a-4974-8335-e3e6aa8979aa']");
+var cubosTexto = document.querySelector("[id='3fa93acd-98a2-40ef-9ce4-88d27246fb0e']");
+var gizTexto = document.querySelector("[id='fbee695e-2b61-4da5-9983-18212becf447']");
+
 
 agaveTexto.setAttribute('data-aos', 'fade-right');
 
 baobaTexto.setAttribute('data-aos', 'fade-left');
 baobaTexto.setAttribute('data-aos-delay', '400');
+
+cubosTexto.setAttribute('data-aos', 'fade-right');
+
+gizTexto.setAttribute('data-aos', 'fade-left');
