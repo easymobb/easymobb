444dc8212cc923edd3da4d4d3894fde4b91000a6
ThiagoMullerR <thiago.m.rodrigues@outlook.com>
2020-08-10 17:48:32 -0300

    js

--- js/coisasdobrasil-script.js
+++ js/coisasdobrasil-script.js
@@ -8,20 +8,18 @@ var hexagonoTexto = document.querySelector("[id='cfcd8285-e37e-46be-9e42-d37ded4
 var jauaTexto = document.querySelector("[id='d570c2aa-5287-4cb5-8e6f-45b584b25d85']");
 var lineTexto = document.querySelector("[id='24dacdd1-ced8-4be3-ab0e-d630c9526840']");
 var pacManTexto = document.querySelector("[id='b8e0ace1-c911-4cb7-bc3f-4b4c98088166']");
-
+var unoDuoTreTexto = document.querySelector("[id='c73b7b4b-0852-4538-b331-3565a6587a6d']");
+var pointTexto = document.querySelector("[id='017d4425-7174-45a9-bcc6-d2943065e980']");
+var boothsTexto = document.querySelector("[id='30be4058-13d7-4b19-be9d-38adcc82c36a']");
 
 agaveTexto.setAttribute('data-aos', 'fade-right');
-
 baobaTexto.setAttribute('data-aos', 'fade-left');
-
 cubosTexto.setAttribute('data-aos', 'fade-right');
-
 gizTexto.setAttribute('data-aos', 'fade-left');
-
 hexagonoTexto.setAttribute('data-aos', 'fade-right');
-
 jauaTexto.setAttribute('data-aos', 'fade-left');
-
 lineTexto.setAttribute('data-aos', 'fade-right');
-
 pacManTexto.setAttribute('data-aos', 'fade-left');
+unoDuoTreTexto.setAttribute('data-aos', 'fade-right');
+pointTexto.setAttribute('data-aos', 'fade-left');
+boothsTexto.setAttribute('data-aos', 'fade-right');
