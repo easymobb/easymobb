2774ef4d981197db67436312d1db7aa6790dc2e1
ThiagoMullerR <thiago.m.rodrigues@outlook.com>
2020-08-10 17:17:41 -0300

    line

--- js/coisasdobrasil-script.js
+++ js/coisasdobrasil-script.js
@@ -6,6 +6,7 @@ var cubosTexto = document.querySelector("[id='3fa93acd-98a2-40ef-9ce4-88d27246fb
 var gizTexto = document.querySelector("[id='fbee695e-2b61-4da5-9983-18212becf447']");
 var hexagonoTexto = document.querySelector("[id='cfcd8285-e37e-46be-9e42-d37ded4d9fa8']");
 var jauaTexto = document.querySelector("[id='d570c2aa-5287-4cb5-8e6f-45b584b25d85']");
+var lineTexto = document.querySelector("[id='24dacdd1-ced8-4be3-ab0e-d630c9526840']");
 
 
 agaveTexto.setAttribute('data-aos', 'fade-right');
@@ -20,3 +21,5 @@ gizTexto.setAttribute('data-aos', 'fade-left');
 hexagonoTexto.setAttribute('data-aos', 'fade-right');
 
 jauaTexto.setAttribute('data-aos', 'fade-left');
+
+lineTexto.setAttribute('data-aos', 'fade-right');
