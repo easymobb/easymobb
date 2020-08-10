04a9786fbb10d3d1f0da4050a46a92bc3a6a2c69
ThiagoMullerR <thiago.m.rodrigues@outlook.com>
2020-08-10 17:32:15 -0300

    js

--- js/coisasdobrasil-script.js
+++ js/coisasdobrasil-script.js
@@ -7,12 +7,12 @@ var gizTexto = document.querySelector("[id='fbee695e-2b61-4da5-9983-18212becf447
 var hexagonoTexto = document.querySelector("[id='cfcd8285-e37e-46be-9e42-d37ded4d9fa8']");
 var jauaTexto = document.querySelector("[id='d570c2aa-5287-4cb5-8e6f-45b584b25d85']");
 var lineTexto = document.querySelector("[id='24dacdd1-ced8-4be3-ab0e-d630c9526840']");
+var pacManTexto = document.querySelector("[id='b8e0ace1-c911-4cb7-bc3f-4b4c98088166']");
 
 
 agaveTexto.setAttribute('data-aos', 'fade-right');
 
 baobaTexto.setAttribute('data-aos', 'fade-left');
-baobaTexto.setAttribute('data-aos-delay', '400');
 
 cubosTexto.setAttribute('data-aos', 'fade-right');
 
@@ -23,3 +23,5 @@ hexagonoTexto.setAttribute('data-aos', 'fade-right');
 jauaTexto.setAttribute('data-aos', 'fade-left');
 
 lineTexto.setAttribute('data-aos', 'fade-right');
+
+pacManTexto.setAttribute('data-aos', 'fade-left');
