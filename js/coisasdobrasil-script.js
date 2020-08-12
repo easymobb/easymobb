94d28554419edf07bc5d7aed12f5d539bc9cd6bd
ThiagoMullerR <thiago.m.rodrigues@outlook.com>
2020-08-12 18:33:56 -0300

    js cdb

--- js/coisasdobrasil-script.js
+++ js/coisasdobrasil-script.js
@@ -9,7 +9,12 @@ var lineTexto = document.querySelector("[id='24dacdd1-ced8-4be3-ab0e-d630c952684
 var pacManTexto = document.querySelector("[id='b8e0ace1-c911-4cb7-bc3f-4b4c98088166']");
 var unoDuoTreTexto = document.querySelector("[id='c73b7b4b-0852-4538-b331-3565a6587a6d']");
 var pointTexto = document.querySelector("[id='017d4425-7174-45a9-bcc6-d2943065e980']");
+var caeteTexto = document.querySelector("[id='fdb9f185-a1c4-4752-9146-4d0f63dc26af']");
+var pippaTexto = document.querySelector("[id='b6ca0e75-89f9-4c9d-9a0f-aa09f740194f']");
 var boothsTexto = document.querySelector("[id='30be4058-13d7-4b19-be9d-38adcc82c36a']");
+var painelDecorativoTexto = document.querySelector("[id='7aea6520-6a5c-4ff4-8d62-e2b97252e19c']");
+var futonsTexto = document.querySelector("[id='388ac013-7fed-43fc-9d46-32a96a3d185d']");
+var BancosEAcessoriosTexto = document.querySelector("[id='72335cc0-5846-4d03-83d0-315e6179d32f']");
 
 agaveTexto.setAttribute('data-aos', 'fade-right');
 baobaTexto.setAttribute('data-aos', 'fade-left');
@@ -21,4 +26,9 @@ lineTexto.setAttribute('data-aos', 'fade-right');
 pacManTexto.setAttribute('data-aos', 'fade-left');
 unoDuoTreTexto.setAttribute('data-aos', 'fade-right');
 pointTexto.setAttribute('data-aos', 'fade-left');
+caeteTexto.setAttribute('data-aos', 'fade-right');
+pippaTexto.setAttribute('data-aos', 'fade-left');
 boothsTexto.setAttribute('data-aos', 'fade-right');
+painelDecorativoTexto.setAttribute('data-aos', 'fade-left');
+futonsTexto.setAttribute('data-aos', 'fade-right');
+BancosEAcessoriosTexto.setAttribute('data-aos', 'fade-left');
